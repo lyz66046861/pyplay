857c75cef4e9ef92ebc99bb240deb56f225784dc
Ward Lu <lyz66046861@gmail.com>
2022-07-03 05:06:30 +0800

    error catch

--- test.py
+++ test.py
@@ -13,3 +13,12 @@ print(os.path.join(os.path.dirname(__file__), os.pardir))
 #accept user input
 #print('Please input your name:')
 #name = input()
+#accept arg from command line
+arg=sys.argv[1]
+# arg = input("ip:")
+#ping 192.168.1 from 1-255 by multi thread
+# os.system("ping -t %s -n %s" % (arg, 255))
+for i in range(1,256):
+    os.system("ping  %s.%s -w 1 -n 1" % (arg,i))
+#    os.system('ping 192.168.3.1 -n 1 %s' % i)
+#    print('ping -n 1 %s' % i)
